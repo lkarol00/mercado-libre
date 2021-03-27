89fb83ae4bcebc504687854f7277eee0f159a6c8
Karol Alzate <karol.alzate@udea.edu.co>
2021-03-26 23:39:52 -0500

    fix env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  items: 'https://api.mercadolibre.com/items/',
+  search: 'https://api.mercadolibre.com/sites/MCO/search?q=',
+  sellers: 'https://api.mercadolibre.com/users/'
 };
